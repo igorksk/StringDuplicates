a7e6db7db6833de59dcdab1d1c9ace9dcd837de0
Igor <ihor.kaseko@aploris.com>
2022-05-17 08:57:38 +0300

    Add project files.

---
+++ StringDuplicates/DuplicatesChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDuplicates
+{
+    internal class DuplicatesChecker
+    {
+        public static bool CheckForDuplicates(string data)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            int index = 0;
+
+            foreach (char symbol in data)
+            {
+                try
+                {
+                    hashtable.Add(symbol, index);
+                }
+                catch
+                {
+                    Console.WriteLine($"An element with Key = {symbol} already exists.");
+                    return true;
+                }
+
+                index++;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ StringDuplicates/Program.cs
@@ -0,0 +1,15 @@
+﻿using StringDuplicates;
+
+Console.WriteLine("Hello, World!");
+
+string test1 = "qwerty";
+
+bool testResult1 = DuplicatesChecker.CheckForDuplicates(test1);
+
+Console.WriteLine("String " + test1 + (testResult1? " contains duplicates" : " doesn't contain duplicates"));
+
+string test2 = "qwertyqu";
+
+bool testResult2 = DuplicatesChecker.CheckForDuplicates(test2);
+
+Console.WriteLine("String " + test2 + (testResult2 ? " contains duplicates" : " doesn't contain duplicates"));
